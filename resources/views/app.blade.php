4b17a38e6ccc60f6900a4a135ccc1864566f1460
unknown <waynedemetra@gmail.com>
2019-12-06 16:29:50 -0800

    Changed loader icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,23 +12,16 @@
         <div class="app-wrapper">
             <div class="loader-container">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-                    <rect x="19" y="19" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0s" calcMode="discrete"></animate>
-                    </rect><rect x="40" y="19" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0.1984126984126984s" calcMode="discrete"></animate>
-                    </rect><rect x="61" y="19" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0.3968253968253968s" calcMode="discrete"></animate>
-                    </rect><rect x="19" y="40" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="1.3888888888888888s" calcMode="discrete"></animate>
-                    </rect><rect x="61" y="40" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0.5952380952380952s" calcMode="discrete"></animate>
-                    </rect><rect x="19" y="61" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="1.1904761904761905s" calcMode="discrete"></animate>
-                    </rect><rect x="40" y="61" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0.9920634920634921s" calcMode="discrete"></animate>
-                    </rect><rect x="61" y="61" width="20" height="20" fill="#50789f">
-                      <animate attributeName="fill" values="#76cbc1;#50789f;#50789f" keyTimes="0;0.125;1" dur="1.5873015873015872s" repeatCount="indefinite" begin="0.7936507936507936s" calcMode="discrete"></animate>
-                    </rect>
+                    <defs>
+                      <clipPath id="ldio-m757uprblfp-cp" x="0" y="0" width="100" height="100">
+                        <rect x="77.956" y="0" width="100" height="100">
+                          <animate attributeName="width" repeatCount="indefinite" dur="1s" values="0;100;100" keyTimes="0;0.5;1"></animate>
+                          <animate attributeName="x" repeatCount="indefinite" dur="1s" values="0;0;100" keyTimes="0;0.5;1"></animate>
+                        </rect>
+                      </clipPath>
+                    </defs>
+                    <path fill="none" stroke="#50789f" stroke-width="2.7928" d="M82 63H18c-7.2 0-13-5.8-13-13v0c0-7.2 5.8-13 13-13h64c7.2 0 13 5.8 13 13v0C95 57.2 89.2 63 82 63z"></path>
+                    <path fill="#76cbc1" clip-path="url(#ldio-m757uprblfp-cp)" d="M81.3 58.7H18.7c-4.8 0-8.7-3.9-8.7-8.7v0c0-4.8 3.9-8.7 8.7-8.7h62.7c4.8 0 8.7 3.9 8.7 8.7v0C90 54.8 86.1 58.7 81.3 58.7z"></path>
                 </svg>
             </div>
         </div>
