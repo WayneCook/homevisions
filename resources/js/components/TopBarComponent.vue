39c1af5f2d324e092681f8fc9dfd1c36d8f5fae5
Wayne Cook <waynedemetra@gmail.com>
2020-03-10 16:00:39 -0700

    Added dynamic company info

--- resources/js/components/TopBarComponent.vue
+++ resources/js/components/TopBarComponent.vue
@@ -1,35 +1,33 @@
 <template>
-    <div class="top-bar-wrapper container-fluid">
+    <div class="top-bar-wrapper container-fluid" v-if='loaded'>
 
-                <div class="top-bar custom-container d-flex d-sm-none justify-end">
-                    <div class="phone-info-container">
-                    <a class="phone-link" href="tel:1-626-531-7066">
-                        <v-icon
-                        size='20'
-                        color='#696b6cba'
-                        >
-                            mdi-phone
-                        </v-icon>
-                        {{ getData.companyInfo.phone }}
-                    </a>
-                    </div>
-                </div>
-
-                <div class="top-bar custom-container d-none d-sm-flex justify-end">
-                    <div class="phone-info-container">
-                    <v-icon
-                    size='20'
-                    color='#696b6cba'
-                    >
-                        mdi-phone
-                    </v-icon>
-                    {{ getData.companyInfo.phone }}
-                    </div>
-                </div>
-
+        <div class="top-bar custom-container d-flex d-sm-none justify-end">
+            <div class="phone-info-container">
+            <a class="phone-link" :href="`tel:${getData.companyInfo.phone}`">
+                <v-icon
+                size='20'
+                color='#696b6cba'
+                >
+                    mdi-phone
+                </v-icon>
+                {{ getData.companyInfo.phone }}
+            </a>
             </div>
+        </div>
 
+        <div class="top-bar custom-container d-none d-sm-flex justify-end">
+            <div class="phone-info-container">
+            <v-icon
+            size='20'
+            color='#696b6cba'
+            >
+                mdi-phone
+            </v-icon>
+            {{ getData.companyInfo.phone }}
+            </div>
+        </div>
 
+    </div>
 </template>
 
 <script>
@@ -38,6 +36,9 @@ export default {
     computed: {
             getData() {
                 return this.$store.getters['app/getData']
+            },
+            loaded() {
+                return this.$store.getters['app/getLoaded']
             }
         }
 }
